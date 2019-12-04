03fdb02dd91b9fbb4c9e9f8b23420a4948f8fadb
Slama Abdelwaheb <Abdelwaheb.Slama@yahoo.com>
2019-12-04 16:37:35 +0100

    Merge remote-tracking branch  ajout des formulaire 'origin/master'

---
+++ src/Form/EnseignantType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Enseignant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EnseignantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('cin')
+            ->add('nom')
+            ->add('prenom')
+            ->add('adresse')
+            ->add('email')
+            ->add('image')
+            ->add('specialite')
+            ->add('CV')
+            ->add('Ecole');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Enseignant::class,
+        ]);
+    }
+}
